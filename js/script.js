2c832a45b7c9264780a420700b71cccc33cf76ed
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2023-10-15 00:58:27 +0330

    finished:You can calculate your BMI

--- js/script.js
+++ js/script.js
@@ -10,9 +10,9 @@ const slider2 = document.getElementById("myHeight");
 let w1 = document.getElementById("w1");
 let h1 = document.getElementById("h1");
 
-let bmi;
-let weight = 80;
-let height = 170;
+let bmi,error,measure;
+let weight = 70;
+let height = 180;
 
 function upDateSlider1() {
     console.log(slider1.value);
@@ -30,5 +30,28 @@ function upDateSlider2() {
 function calculate(){
     height /= 100;
     height *= height;
+    bmi = weight / height;
+    bmi = bmi.toFixed(1);
 
+    if (bmi < 18.5){
+        measure = "your BMI is" + bmi + " which means " + "you are Underweight";
+    } else if (bmi < 25) {
+        measure = "your BMI is" + bmi + " which means " + "you are Normal"
+    } else if (bmi < 30) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
+	} else if (bmi >= 30) {
+		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
+	}
+
+    if (weight === 0 ) {
+		document.getElementById("results").innerHTML = error;
+	} else if (height === 0){
+		document.getElementById("results").innerHTML = error;
+	}
+	 else {
+		document.getElementById("results").innerHTML = measure;
+	}
+	if (weight < 0) {
+		document.getElementById("results").innerHTML = "Negative Values not Allowed";
+	}
 }
