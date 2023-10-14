f9275b13f2fde3ded3f995b1ee290a996185ebae
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2023-10-14 23:19:45 +0330

    changed slider and get value

--- js/script.js
+++ js/script.js
@@ -5,8 +5,24 @@
 // weight.addEventListener('range', () => {
     // console.log(weight.value);
 // })
-const slider = document.getElementById("myWeight");
-function upDateSlider() {
-    console.log(slider.value);
-    
+const slider1 = document.getElementById("myWeight");
+const slider2 = document.getElementById("myHeight");
+let w1 = document.getElementById("w1");
+let h1 = document.getElementById("h1");
+
+let weight = 80;
+let height = 170;
+
+function upDateSlider1() {
+    console.log(slider1.value);
+    weight = slider1.value;
+    w1.innerHTML = slider1.value;
+
 }
+
+function upDateSlider2() {
+    console.log(slider2.value);
+    height = slider2.value;
+    h1.innerHTML = slider2.value; 
+}
+
