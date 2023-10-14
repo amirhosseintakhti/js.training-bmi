ffb69cf4f2500c18b1499f27493562580205c920
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2023-10-14 18:08:16 +0330

    bmi

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// const weight = document.getElementById("myWeight");
+// let show = document.getElementById("w1")
+// 
+// weight.addEventListener('range', () => {
+    // console.log(weight.value);
+// })
+const slider = document.getElementById("myWeight");
+function upDateSlider() {
+    
+}
